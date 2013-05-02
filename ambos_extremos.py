402edb81084b2f1307df3151bac1f47826eeb98f
xelomi <xelomi@xelomi.(none)>
2013-05-02 15:35:56 -0400

    funciona raices

--- ambos_extremos.py
+++ ambos_extremos.py
@@ -1,3 +1,7 @@
+#Nombre: 	Marcelo Minay
+#Actividad: Realizar un programa que dado un arreglo de caracteres tome las dos primeras y dos ultimas letras 
+#			genere una nueva palabra con estas.
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Declaración de funciones
 #~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -5,10 +9,10 @@
 
 def ambos_extremos(n):
     vacio=""
-    if len(n)<2:
+    if len(n)<2:         #verifico si el largo del arreglo es mayor a 2 en caso de ser menor retorno un string vacio
         return vacio
     else:
-        return n[0:2] + n[-2] + n[-1]
+        return n[0:2] + n[-2] + n[-1]		#Tomar las 2 primeras letras y luego concatenar con las dos últimas
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Declaración de funcion principal
